2a17dfb42fdc0af178294ffa7eb67cac606cec82
Francesca Mendoza <125602557+francemc@users.noreply.github.com>
2024-04-07 20:15:19 +0200

    TransferCliente bonito

--- src/Presentacion/Main.java
+++ src/Presentacion/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Crear una instancia de Controlador
-        Controlador controlador = Controlador.getInstancia(); // Asegúrate de ajustar según tu implementación
+        Controlador controlador = Controlador.getInstancia(); 
 
         // Pasar el controlador al constructor de GUIClientesImp
         GUIClientesImp clientesImp = new GUIClientesImp(controlador);
